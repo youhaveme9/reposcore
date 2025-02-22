39224bc107db4ab8dcfcc8f5521b8a2fa28429db
Roshan Kumar <roshaen09@gmail.com>
2025-02-22 20:25:27 +0530

    [change] Minor refactors

--- reposcore/main.py
+++ reposcore/main.py
@@ -1,29 +1,30 @@
 import os
 from reposcore.utils import Utils
-from reposcore.scanners.dependency import checkSecurityDependency, checkOutdatedDependency
+from reposcore.scanners.dependencyScanner import checkSecurityDependency, checkOutdatedDependency
 from reposcore.scanners.secretsScanner import checkLeaks
 from reposcore.scanners.activityScanner import getActivityScore
 
 utils = Utils()
+REPO_PATH = os.path.join(os.getcwd(), 'temp', 'repo')
 
 def main(url: str):
     utils.heading()
     utils.cloneRepo(url)
 
     # score for dependency security
-    dependencyScore = checkSecurityDependency(os.path.join(os.getcwd(), 'temp', 'repo'))
+    dependencyScore = checkSecurityDependency(REPO_PATH)
     utils.printTitle('Dependency security score')
     utils.printText(f"Score: {dependencyScore}/50")
     print()
 
     # score for outdated dependencies
-    outdatedScore = checkOutdatedDependency(os.path.join(os.getcwd(), 'temp', 'repo'))
+    outdatedScore = checkOutdatedDependency(REPO_PATH)
     utils.printTitle('Outdated Dependency score')
     utils.printText(f"Score: {outdatedScore}/20")
     print()
 
     # score for leaked secrets
-    secretsScore = checkLeaks(os.path.join(os.getcwd(), 'temp', 'repo'))
+    secretsScore = checkLeaks(REPO_PATH)
     print()
     utils.printTitle('Leaked secrets score')
     utils.printText(f"Score: {secretsScore}/20")
